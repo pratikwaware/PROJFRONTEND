c29a7b4683cf4064b49f4f71eace91a45f43b5ab
pratikwaware <pratikwavare919@gmail.com>
2022-09-25 20:45:20 +0530

    uncomment 4 d

--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -25,7 +25,6 @@ const SignUp = () => {
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    // eslint-disable-next-line no-console
     console.log({
       email: data.get("email"),
       password: data.get("password"),
